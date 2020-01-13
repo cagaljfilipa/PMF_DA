50ea2a82786ec0bb8c081664526fb064d83cd6e0
Filipa Cagalj <filipa.cagalj@gmail.com>
2020-01-13 18:00:17 +0100

    create-read database

--- PMF_DatingApp-master/app.py
+++ PMF_DatingApp-master/app.py
@@ -3,13 +3,18 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
-
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = 'sqlite:///{}'.format(os.path.join(project_dir, 'db.sqlite3'))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
 
 db = SQLAlchemy(app)
 
@@ -17,8 +22,11 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
-    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Username: {}>'.format(self.username)
 
 
 @app.route('/')
@@ -26,17 +34,29 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=["GET","POST"])
+@app.route('/login')
 def login():
-    if request.form:
-        print(request.form)
-        
+
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    if request.form:
+        name = request.form['username']
+
+        #username = User(username=request.form.get('username'))
+        #email = User(email=request.form.get('email'))
+        #password = User(password=request.form.get('password'))
+
+        signature = User(username=name)
+        db.session.add(signature)
+        #db.session.add_all([username, email, password])
+        db.session.commit()
+        print(request.form)
+
+    users = User.query.all()
+    return render_template('signup.html', username=users)
 
 
 @app.route('/dashboard')
